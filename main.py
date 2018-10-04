f07cc28efbd94cf1a9c0969ceb1cee862addd6c2
yurasolovjov <yura.solovjov@mail.ru>
2018-10-04 10:52:15 +0300

    Change algorithm of generate data

--- main.py
+++ main.py
@@ -19,30 +19,56 @@ header = ["INPUT:session1_text",
           "INPUT:session4_number"
           ]
 
-def getRandomSample(len_dictionary, len_sequence = 10):
+common_phrase = int(0)
+common_number = str("1 1 2 3 5")
+
+def getRandomSample(len_sequence = 10):
 
     sequence = [x for x in range(len_sequence)]
 
     random.shuffle(sequence)
-    sample = random.sample(sequence,k=random.randint(len_dictionary, len_dictionary * 2))
+    sample = random.sample(sequence,k=5)
 
     str_sample = str("")
 
     for i in sample:
         str_sample += '{0} '.format(i)
 
-    return str_sample
+    return converNumberStringToWordString(str_sample)
 
+def getRandomPhrase(library,phrase = "", length = 5, id = None):
 
-def getRandomPhrase(library):
-    phrase = str("")
-
-    r = random.randint(0,len(library))
+    if id is None:
+        r = random.randint(0,len(library) - 10)
+    else:
+        r = int(id)
 
     if (len(library[r]) > 0):
-        phrase = library[r]
+        phrase += library[r]
 
-    return phrase
+    phrase_processed = phrase.split(" ")
+
+    counter = int(0)
+
+    for p in phrase_processed:
+
+        if checkWord(p,3) == True:
+            counter +=1
+
+    try:
+        if counter < length:
+            phrase = getRandomPhrase(library = library, phrase = phrase, length = length, id = r + 1)
+    except:
+        raise  Exception("*** ERROR ***")
+
+    return phrase.lower().replace('\n',' ')
+
+def checkWord(word, length):
+
+    if len(word) >= length :
+        return True
+    else:
+        return False
 
 def getListBooks(catalog):
 
@@ -52,6 +78,52 @@ def getListBooks(catalog):
 
     return listFiles
 
+def converNumberToWord(number):
+
+    word = str("")
+
+    number = int(number)
+
+    if number == 0:
+        word = "ноль"
+    elif number == 1:
+        word = str("один")
+    elif number == 2:
+        word = str("два")
+    elif number == 3:
+        word = str("три")
+    elif number == 4:
+        word = str("четыре")
+    elif number == 5:
+        word = str("пять")
+    elif number == 6:
+        word = str("шесть")
+    elif number == 7:
+        word = str("семь")
+    elif number == 8:
+        word = str("восемь")
+    elif number == 9:
+        word = str("девять")
+    else:
+        word = str("девять")
+
+    return word
+
+def converNumberStringToWordString(phrase):
+
+    phrase = str(phrase).replace(" ","")
+
+    new_phrase = str("")
+
+    for number in phrase:
+
+        word = converNumberToWord(number)
+
+        new_phrase += word + str(", ")
+
+    new_phrase = new_phrase[:-2]
+
+    return new_phrase
 
 
 def main():
@@ -60,6 +132,7 @@ def main():
     parser.add_argument("-i", "--input", help="Catalog of books. Catalog should include txt files", action="append", default=None, nargs="*")
     parser.add_argument("-o", "--output", help="Output catalog", default="./model_toloka")
     parser.add_argument("-l", "--len_dictionary", help="Length of dictionary", type=int, default=5)
+    parser.add_argument("-s", "--shuffle", help="Shuffle of data", default=False)
 
 
     args = parser.parse_args()
@@ -68,6 +141,7 @@ def main():
     input_list_catalogs = args.input
     inputCatalogs = list()
     len_dictionary = int(args.len_dictionary)
+    shuffle_data = bool(args.shuffle)
 
     if input_list_catalogs is None:
         raise Exception("Input catalog is None")
@@ -99,14 +173,15 @@ def main():
         if len(line) < 2:
             library.remove(line)
 
-    random.shuffle(library)
-
+    if shuffle_data == True:
+        random.shuffle(library)
 
     count_line = len(library)
     count = int(0)
 
     for line in library:
 
+        line = getRandomPhrase(library=library, length=len_dictionary)
 
         try:
             line_data = {"number_1": getRandomSample(len_dictionary),
@@ -114,9 +189,9 @@ def main():
                          "number_2": getRandomSample(len_dictionary),
                          "phrase_2": line,
                          "number_3": getRandomSample(len_dictionary),
-                         "phrase_3": getRandomPhrase(library),
-                         "number_4": getRandomSample(len_dictionary),
-                         "phrase_4": getRandomPhrase(library)
+                         "phrase_3": getRandomPhrase(library=library, length=len_dictionary),
+                         "number_4": converNumberStringToWordString(common_number),
+                         "phrase_4": getRandomPhrase(library=library, length=len_dictionary, id = common_phrase)
                          }
         except:
             print("*** ERROR ***")
